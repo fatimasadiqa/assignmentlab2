2ae5b0d6108f802006ec8a4b4b479a7d1b30d303
naqvi0512 <muneeza2019@namal.edu.pk>
2021-10-04 19:59:15 +0500

    square function

--- fatimacal01.py
+++ fatimacal01.py
@@ -4,6 +4,8 @@ def sub(num1, num2):
        return num1 - num2
 def add(num1,num2):
       return num1+num2
+def squAre(num1,num2):
+      return num1**num2
 # main
-a = (sub(6, 2),add(6,3))
+a = (sub(6, 2),add(6,3),squAre(4,7))
 print(a)
